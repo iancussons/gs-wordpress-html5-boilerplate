8422702cdb93c90d6f4964ec7956b766568334ad
Simon MacFadyen <simon.macfadyen@greensplash.com>
2012-11-28 15:55:48 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
-
-require_once locate_template('/includes/init.php'); 				// Theme setup, initial creations
-require_once locate_template('/includes/scripts.php');				// CSS and JS
+require_once locate_template('/includes/init.php'); 				      // Theme setup, initial creations
+require_once locate_template('/includes/scripts.php');				    // CSS and JS
 require_once locate_template('/includes/custom-functions.php');		// Custom functions
 ?>
